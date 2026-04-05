c09caa7bee866ae1c2450a06f3bcc2e6766ac0af
RWAS <rwas@RWASs-Mac-mini.localdomain>
2026-04-05 19:11:06 -0400

    Fix live homepage: Garmin Store sidebar + 1200px width

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,11 @@ async function loadNewspaperHtml() {
     .replace(/src="images\//g, 'src="/newspaper/images/')
     .replace(/src='images\//g, "src='/newspaper/images/")
     .replace(/href="images\//g, 'href="/newspaper/images/')
-    .replace(/href='images\//g, "href='/newspaper/images/");
+    .replace(/href='images\//g, "href='/newspaper/images/")
+    .replace(
+      /<div class="box-row"><a href="https:\/\/rogerwilcoaviation\.com\/collections\/garmin-avionics" style="text-decoration:none;color:inherit;display:contents;"><span>G3X Touch Suite<\/span><span class="box-pg">A3 &rarr;<\/span><\/a><\/div>/,
+      '<div class="box-row"><a href="/garmin" style="text-decoration:none;color:inherit;display:contents;"><span>Garmin Store</span><span class="box-pg">A3 &rarr;</span></a></div>',
+    );
 
   return { style, body };
 }
