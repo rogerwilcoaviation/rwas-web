afe1168347e6dfb0c3fda5f99cc14c695c69b161
Jerry (RWAS Mac Mini) <jerry@rwas.team>
2026-08-24 03:56:41 -0400

    Use supplied photo for AXIS GDL remote receivers

--- components/shopify/AxisBuildPlanner.tsx
+++ components/shopify/AxisBuildPlanner.tsx
@@ -42,6 +42,18 @@ const EXACT_PRODUCT_IMAGES: Record<
     imageAlt:
       'Garmin GA 24 TNC SiriusXM antenna with attached cable and TNC connector',
   },
+  '010-01561-15': {
+    imageUrl: '/images/axis/products/gdl-50r-51r-52r-remote-receiver.jpg',
+    imageAlt: 'Garmin GDL 50R remote ADS-B receiver',
+  },
+  '010-01561-35': {
+    imageUrl: '/images/axis/products/gdl-50r-51r-52r-remote-receiver.jpg',
+    imageAlt: 'Garmin GDL 52R remote SiriusXM and ADS-B receiver',
+  },
+  '010-01561-55': {
+    imageUrl: '/images/axis/products/gdl-50r-51r-52r-remote-receiver.jpg',
+    imageAlt: 'Garmin GDL 51R remote SiriusXM receiver',
+  },
   '010-04557-00': {
     imageUrl: '/images/axis/products/010-04557-00-certified-lru-kit.jpg',
     imageAlt:
